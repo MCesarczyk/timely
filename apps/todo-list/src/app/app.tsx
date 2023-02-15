8880efc45dadce86f4380da6da24fc2fbea6d96c
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2023-02-15 23:50:48 +0100

    Initial commit

---
+++ apps/todo-list/src/app/app.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+import NxWelcome from './nx-welcome';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <NxWelcome title="todo-list" />
+    </StyledApp>
+  );
+}
+
+export default App;
