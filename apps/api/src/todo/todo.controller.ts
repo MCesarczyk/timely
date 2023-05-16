6599d789a6c3aae33cc5b7fac4151a6f0aa43bab
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2023-05-17 00:58:08 +0200

    feat(todos): add C U D to module

--- apps/api/src/todo/todo.controller.ts
+++ apps/api/src/todo/todo.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { todos } from './todos-mock';
 import { TodoDTO } from './todo.dto';
 
@@ -9,5 +9,31 @@ export class TodoController {
   @Get()
   getTodos(): TodoDTO[] {
     return todosData;
+  }
+
+  @Post()
+  createTodo(@Body() createTodo: TodoDTO): TodoDTO {
+    const newTodo: TodoDTO = {
+      id: (todosData.length + 1).toString(),
+      ...createTodo
+    };
+
+    todosData = [...todosData, newTodo];
+
+    return newTodo;
+  }
+
+  @Put(':id')
+  updateTodo(@Body() updateTodo: TodoDTO, @Param('id') id): TodoDTO {
+    todosData = todosData.map(todo => (todo.id === id ? updateTodo : todo));
+    return updateTodo;
+  }
+
+  @Delete(':id')
+  deleteTodo(@Param('id') id): TodoDTO {
+    const todoToDelete = todosData.find(todo => todo.id === id);
+    todosData = todosData.filter(todo => todo.id !== id);
+
+    return todoToDelete;
   }
 }
