1f5463cda6ddb56f0e3774842ac9b51fc9f5fb7b
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2023-05-30 01:26:20 +0200

    feat(react_query): install package, implement in app

--- apps/todo-list/src/index.tsx
+++ apps/todo-list/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 import { Normalize } from 'styled-normalize';
@@ -8,23 +9,30 @@ import { App } from 'app/App';
 import { store } from 'store';
 import { theme } from 'theme';
 import { GlobalStyle } from 'GlobalStyle';
-// import reportWebVitals from 'reportWebVitals';
+import { DEFAULT_CACHE_TIME, DEFAULT_STALE_TIME } from 'common/constants';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      keepPreviousData: true,
+      staleTime: DEFAULT_STALE_TIME,
+      cacheTime: DEFAULT_CACHE_TIME,
+    },
+  },
+});
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <Normalize />
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
-    </Provider>
-  </React.StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Normalize />
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </Provider>
+    </QueryClientProvider>
+  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
