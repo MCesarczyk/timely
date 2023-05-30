928ec24d1a3541dadc9ca1344ea16bff5b20751b
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2023-05-30 06:56:29 +0200

    build(react_query): migrate to tanstack query

--- apps/todo-list/src/app/tasks/tasksApiService.ts
+++ apps/todo-list/src/app/tasks/tasksApiService.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery } from "@tanstack/react-query";
 import { restApi } from "services/restApi";
 import { isTaskListValid, isTaskValid } from "./typeguards";
 
