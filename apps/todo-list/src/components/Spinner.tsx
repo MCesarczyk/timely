18e92cd035a58b33b0d9c1a0215d2ee24e82ef79
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2023-06-04 20:21:41 +0200

    feat(author): add spinner wrapper to github repos list

---
+++ apps/todo-list/src/components/Spinner.tsx
@@ -0,0 +1,46 @@
+import { ReactComponent as LoaderCircle } from 'assets/svg/loaderCircle.svg';
+
+interface SpinnerProps {
+  caption?: string;
+}
+
+export const Spinner = ({ caption }: SpinnerProps) => (
+  <>
+    <LoaderCaption>{caption || 'Loading...'}</LoaderCaption>
+    <LoaderCircleWrapper>
+      <SpinnerCircle />
+    </LoaderCircleWrapper>
+  </>
+);
+
+import styled from 'styled-components';
+
+export const LoaderCaption = styled.p`
+  font-size: 20px;
+  margin-top: 36px;
+`;
+
+export const LoaderCircleWrapper = styled.div`
+  width: 100px;
+  aspect-ratio: 1;
+  margin: 24px;
+`;
+
+export const SpinnerCircle = styled(LoaderCircle)`
+  width: 100%;
+  height: 100%;
+  color: teal;
+
+  @media (prefers-reduced-motion: no-preference) {
+    animation: Element-spin 1.2s linear infinite;
+  }
+
+  @keyframes Element-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
