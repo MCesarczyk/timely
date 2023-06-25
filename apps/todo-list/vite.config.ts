ba2b1591ab1e01a4a6418da7c82ee9b905d77815
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2023-06-25 04:39:45 +0200

    chore(config): change port to avoid collisions, update connection params

--- apps/todo-list/vite.config.ts
+++ apps/todo-list/vite.config.ts
@@ -18,7 +18,9 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://localhost:4000',
+        changeOrigin: true,
         secure: false,
+        ws: true,
       },
     },
   },
