5502f9b450a92c3d7ab998ad76d7efc11e80cc59
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2025-05-25 18:07:59 +0200

    fix(toto-list): update svg props in react component

--- apps/todo-list/src/components/Spinner.tsx
+++ apps/todo-list/src/components/Spinner.tsx
@@ -11,13 +11,13 @@ const LoaderCircle = (props: SVGProps<SVGSVGElement>) => (
     <path
       d="M80 154.312C121.042 154.312 154.312 121.042 154.312 80C154.312 38.9583 121.042 5.6875 80 5.6875C38.9583 5.6875 5.6875 38.9583 5.6875 80C5.6875 121.042 38.9583 154.312 80 154.312Z"
       stroke="#D1D5DA"
-      stroke-opacity="0.3"
-      stroke-width="11.375"
+      strokeOpacity="0.3"
+      strokeWidth="11.375"
     />
     <path
       d="M132.547 27.4531C146.483 41.3894 154.312 60.2911 154.312 80C154.312 99.7089 146.483 118.611 132.547 132.547"
       stroke="currentColor"
-      stroke-width="11.375"
+      strokeWidth="11.375"
     />
   </svg>
 );
