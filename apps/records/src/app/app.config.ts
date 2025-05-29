05b306edaf801b8063d079465c204a8dc18f0bfd
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2025-05-30 00:21:27 +0200

    feat(records): install tanstack query

--- apps/records/src/app/app.config.ts
+++ apps/records/src/app/app.config.ts
@@ -1,10 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {
+  provideTanStackQuery,
+  QueryClient,
+} from '@tanstack/angular-query-experimental'
+
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideTanStackQuery(new QueryClient())
   ],
 };
