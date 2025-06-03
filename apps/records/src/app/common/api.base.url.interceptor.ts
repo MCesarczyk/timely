7ba3674ba5176986c85f53b12d768a56a90599a9
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2025-06-03 15:20:56 +0200

    ci(records): add local env config

--- apps/records/src/app/common/api.base.url.interceptor.ts
+++ apps/records/src/app/common/api.base.url.interceptor.ts
@@ -1,7 +1,8 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { environment } from 'apps/records/src/environments/environment';
 
 export const ApiBaseUrlInterceptor: HttpInterceptorFn = (req, next) => {
-  const baseUrl = 'http://localhost:4000';
+  const baseUrl = environment.apiUrl;
   const url = req.url.startsWith('http') ? req.url : `${baseUrl}${req.url}`;
   const apiReq = req.clone({ url });
   return next(apiReq);
